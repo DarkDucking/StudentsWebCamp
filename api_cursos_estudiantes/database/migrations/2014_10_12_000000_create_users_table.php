575ac59bb12756c69e71710cc4180912785c4082
DarkDucking <segundoleoshadow@gmail.com>
2024-01-17 18:15:23 -0600

    6.1 con migraciones

--- api_cursos_estudiantes/database/migrations/2014_10_12_000000_create_users_table.php
+++ api_cursos_estudiantes/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 250);
+            $table->string('surname', 250)->nullable();
+            $table->string('email', 255)->unique();
+            $table->string('avatar', 250)->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('state')->unsigned()->default(1);
+            $table->tinyInteger('type_user')->unsigned()->default(1);
             $table->rememberToken();
             $table->timestamps();
         });

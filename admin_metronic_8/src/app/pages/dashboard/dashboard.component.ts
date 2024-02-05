b8083990eadea2ac973d530a3bffa43c7e5e228f
DarkDucking <segundoleoshadow@gmail.com>
2024-02-05 09:09:16 -0600

    node chart

--- admin_metronic_8/src/app/pages/dashboard/dashboard.component.ts
+++ admin_metronic_8/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from 'src/app/modules/course/service/course.service';
+import { UserService } from 'src/app/modules/user/service/user.service';
+
+declare var $:any;
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  data: any;
+  COURSES:any = [];
+  USERS:any = [];
+  isLoading:any;
 
-  ngOnInit(): void {}
+  search:any = null;
+  state:any = null;
+
+  constructor(
+    private courseService: CourseService,
+    private userService: UserService,
+    ) {}
+
+  ngOnInit(): void {
+    this.isLoading = this.courseService.isLoading$;
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.courseService.listCourse(this.search,this.state).subscribe((resp:any) => {
+      console.log(resp);
+      this.COURSES  = resp.courses.data;
+    })
+    this.userService.listUsers(this.search,this.state).subscribe((resp:any) => {
+      console.log(resp);
+      this.USERS  = resp.users.data;
+    })
+  }
 }
--- admin_metronic_8/src/app/pages/dashboard/dashboard.module.ts
+++ admin_metronic_8/src/app/pages/dashboard/dashboard.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { WidgetsModule } from '../../_metronic/partials';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+
     RouterModule.forChild([
       {
         path: '',
